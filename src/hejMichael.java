8be6c408e9f8c3e899c654dc189404b9e370176d
Michael <mich279q@edu.easj.dk>
2017-09-28 13:49:14 +0200

    Initial commit

---
+++ src/hejMichael.java
@@ -0,0 +1,10 @@
+public class hejMichael {
+    public static void main(String[] args) {
+        hejMichael();
+    }
+
+    public static void hejMichael() {
+        System.out.println("Hej András");
+        System.out.println("Hilsen Michael");
+    }
+}
